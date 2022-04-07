719578e150c8c7f9e4fa1fff94ff98d1054f0f87
itsagift <anna.c.reynolds@gmail.com>
2022-04-07 18:01:47 -0400

    added posts request

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
+
+//useEffect = allows you to do effects when something is loaded. RUN IT IN COMPONEN!
 const Timeline = () => {
+    const [posts, setPosts] = useState([])
+    useEffect(async () => {
+        console.log("Rendering timeline")
+        let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json");
+        let res = await req.json();
+        setPosts(res);
+    }, [])
     return (
         <div>
             <h2>Timeline</h2>
+            {
+                posts.map((element) => <h1></h1>)
+            }
         </div>
     )
 }
