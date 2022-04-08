a9a37a96fc4ca1cafdc92b880da1b0d47d39b65c
itsagift <anna.c.reynolds@gmail.com>
2022-04-08 13:25:17 -0400

    added posts to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,21 +2,29 @@ import { useEffect, useState } from 'react';
 
 //useEffect = allows you to do effects when something is loaded. RUN IT IN COMPONEN!
 const Timeline = () => {
-    const [posts, setPosts] = useState([])
-    useEffect(async () => {
-        console.log("Rendering timeline")
-        let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json");
-        let res = await req.json();
-        setPosts(res);
-    }, [])
-    return (
-        <div>
-            <h2>Timeline</h2>
-            {
-                posts.map((element) => <h1></h1>)
-            }
-        </div>
-    )
+  const [posts, setPosts] = useState([])
+  useEffect( ()=> {
+    ( async () => {
+      let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json");
+      let res = await req.json();
+      setPosts(res)
+    }) ()
+  }, [])
+  return (
+    <div>
+      <h2>Timeline</h2>
+        {
+          posts.map((element) => {
+            return(
+              <div>
+                <p>Post by {element.username}</p>
+                <p>{element.content}</p>
+              </div>
+            )
+          })
+        }
+    </div>
+  )
 }
 
 export default Timeline;
