25990368cec29f939789112fce28930964280a46
itsagift <anna.c.reynolds@gmail.com>
2022-04-08 13:57:52 -0400

    added new post button

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,6 +4,7 @@ import Post from './Post';
 //useEffect = allows you to do effects when something is loaded. RUN IT IN COMPONEN!
 const Timeline = () => {
   const [posts, setPosts] = useState([])
+  const [newPostModalVisible, setNewPostModalVisible] = useState(false);
   useEffect( ()=> {
     ( async () => {
       let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json");
@@ -14,6 +15,12 @@ const Timeline = () => {
   return (
     <div>
       <h2>Timeline</h2>
+      <button onClick= {() => {setNewPostModalVisible(!newPostModalVisible)}}>+ New Post</button>
+      <div className={newPostModalVisible ? "create-post-modal" : "create-post-modal-hidden"}>
+        <h3>Create Post</h3>
+        <textarea rows="5" cols="30"/>
+        <button>Create Post</button>
+      </div>
         {
           posts.map((post) => {
             return(
